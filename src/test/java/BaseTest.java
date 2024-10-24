4ea35f44a0bd4bad3f1e0bbe7fe349e3a102a46b
Szymon Tadeusiak <stadeusiak@gmail.com>
2024-10-24 15:13:11 +0200

    PW2 commit2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,20 +6,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Properties;
 import java.util.Objects;
 
 @Slf4j
 abstract class BaseTest {
 
-    protected static final String BASE_URL = "https://www.saucedemo.com/";
-    protected static final String LOCAL_CHROME_PATH = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+//    protected static final String BASE_URL = "https://www.saucedemo.com/";
+//    protected static final String LOCAL_CHROME_PATH = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+
+    private String BASE_URL;
+    private String LOCAL_CHROME_PATH;
     protected Playwright playwright;
     protected Browser browser;
     protected Page page;
 
     @BeforeEach
     public void setupTest() {
+        loadProperties();
         playwright = Playwright.create();
         var launchOptions = new BrowserType.LaunchOptions();
 
@@ -48,6 +54,21 @@ abstract class BaseTest {
         }
         if (playwright != null) {
             playwright.close();
+        }
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try (var input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Can't find config.properties");
+            }
+            properties.load(input);
+            BASE_URL = properties.getProperty("base.url");
+            LOCAL_CHROME_PATH = properties.getProperty("chrome.path");
+        } catch (IOException e) {
+            log.error("Error during properties loading.", e);
+            throw new RuntimeException("Error during properties loading.", e);
         }
     }
 }
